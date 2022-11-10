dbbe340b5ff28d7c9d000b4e1d6f59a232fd6e04
teraiken <terai.tis2022@gmail.com>
2022-11-11 01:21:26 +0900

    更新：LoginController

--- src/main/java/jp/co/sss/shop/controller/login/LoginController.java
+++ src/main/java/jp/co/sss/shop/controller/login/LoginController.java
@@ -69,7 +69,7 @@ public class LoginController {
 			Integer authority = ((UserBean) session.getAttribute("user")).getAuthority();
 			if (authority.intValue() == 2) {
 				// 一般会員ログインした場合、トップ画面に遷移
-				return "";
+				return "index";
 			}
 			else {
 				// 運用管理者、もしくはシステム管理者としてログインした場合、管理者用メニュー画面に遷移
