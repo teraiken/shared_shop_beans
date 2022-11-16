0b09fe8a97efceaded926a8cdf39d2c3dc536190
KeisaburoAbe <keisaburo.abe.tis2022@gmail.com>
2022-11-16 09:46:49 +0900

    更新:JPQLConstant.java 売れ筋検索追加

--- src/main/java/jp/co/sss/shop/util/JPQLConstant.java
+++ src/main/java/jp/co/sss/shop/util/JPQLConstant.java
@@ -10,5 +10,9 @@ public class JPQLConstant {
 	// 注文情報を全件検索(新着順)
 	public static final String	FIND_ALL_ORDERS_ORDER_BY_INSERT_DATE
 		= "SELECT o FROM Order o ORDER BY o.insertDate DESC, o.id ASC";
-
-}
+	
+	//売れ筋商品検索
+	public static final String FIND_ORDER_BY_ORDER_COUNT
+		= "SELECT new Item(i.id, i.name, i.price, i.description, i.image, c.name) FROM Item i INNER JOIN i.category c INNER JOIN i.orderItemList oi WHERE i.deleteFlag = 0 GROUP BY i.id, i.name, i.price, i.description, i.image, c.name ORDER BY COUNT(i.id) DESC, i.id ASC";
+	
+	
