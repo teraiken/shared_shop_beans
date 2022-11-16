7151c0e71b4336bdecf9bf8ff7f88acbf20ca5c6
KeisaburoAbe <keisaburo.abe.tis2022@gmail.com>
2022-11-16 09:45:54 +0900

    更新：ItemRepository.java 売れ筋検索追加

--- src/main/java/jp/co/sss/shop/repository/ItemRepository.java
+++ src/main/java/jp/co/sss/shop/repository/ItemRepository.java
@@ -3,9 +3,12 @@ package jp.co.sss.shop.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.util.JPQLConstant;
 
 /**
  * itemsテーブル用リポジトリ
@@ -19,5 +22,8 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	/**  商品情報を新着順で検索*/
 	public List<Item> findByDeleteFlagOrderByInsertDateDescIdAsc(int deleteFlag);
 
+	/** 商品情報を売れ筋順で検索*/
+	@Query(JPQLConstant.FIND_ORDER_BY_ORDER_COUNT)
+	public List<Item> findAllOrderById();
 	
-}
+	
