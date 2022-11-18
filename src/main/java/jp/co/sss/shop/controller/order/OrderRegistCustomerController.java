6fcc5d030b08482b7a609abb9f28b5e051d1a211
teraiken <terai.tis2022@gmail.com>
2022-11-18 11:43:06 +0900

    修正：OrderRegistCustomerController

--- src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sss.shop.bean.BasketBean;
 import jp.co.sss.shop.bean.OrderItemBean;
@@ -47,34 +48,44 @@ public class OrderRegistCustomerController {
 	OrderItemRepository orderItemRepository;
 	
 	@RequestMapping(path = "/address/input", method = RequestMethod.POST)
-	public String inputAssress(@ModelAttribute OrderForm orderForm, boolean backflg, Model model) {
+	public String inputAddress(@ModelAttribute OrderForm orderForm, boolean backflg, Model model) {
+		
 		if (!backflg) {
 			UserBean userBean = (UserBean)session.getAttribute("user");
 			User user = userRepository.getById(userBean.getId());
-			userBean.setPostalCode(user.getPostalCode());
-			userBean.setAddress(user.getAddress());
-			userBean.setPhoneNumber(user.getPhoneNumber());
-			model.addAttribute("orderForm", userBean);
+			orderForm.setPostalCode(user.getPostalCode());
+			orderForm.setAddress(user.getAddress());
+			orderForm.setName(user.getName());
+			orderForm.setPhoneNumber(user.getPhoneNumber());
+			model.addAttribute("orderForm", orderForm);
 		}
 		return "order/regist/order_address_input";
 	}
 	
+	@RequestMapping(path = "/address/input")
+	public String inputAddressRedirect() {
+		
+		return "order/regist/order_address_input";
+	}
+	
 	@RequestMapping(path = "/payment/input", method = RequestMethod.POST)
-	public String inputPayment(@Valid @ModelAttribute OrderForm orderForm, BindingResult result, Model model) {
+	public String inputPayment(@Valid @ModelAttribute OrderForm orderForm, BindingResult result, RedirectAttributes redirectAttributes) {
+		
 		if (result.hasErrors()) {
-			return "order/regist/order_address_input";
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.orderForm", result);
+			redirectAttributes.addFlashAttribute("orderForm", orderForm);
+			return "redirect:/address/input";
 		}
 		return "order/regist/order_payment_input";
 	}
 	
 	@RequestMapping(path = "/order/check", method = RequestMethod.POST)
-	public String inputPayment(@ModelAttribute OrderForm orderForm, Model model) {
+	public String checkOrder(@ModelAttribute OrderForm orderForm, Model model) {
+		
 		List<BasketBean> basketBeanList = (ArrayList<BasketBean>)session.getAttribute("basketBeans");
 		List<BasketBean> toRemove = new ArrayList<BasketBean>();
-		List<OrderItemBean> orderItemBeanList = new ArrayList<OrderItemBean>();
-		Integer total = 0;
-		List<String> stockShortages = new ArrayList<String>();
 		List<String> stockZeros = new ArrayList<String>();
+		List<String> stockShortages = new ArrayList<String>();
 		for (BasketBean basketBean : basketBeanList) {
 			Item item = itemRepository.getById(basketBean.getId());
 			if (item.getStock() == 0) {
@@ -83,14 +94,14 @@ public class OrderRegistCustomerController {
 			} else if (item.getStock() < basketBean.getOrderNum()) {
 				basketBean.setOrderNum(item.getStock());
 				stockShortages.add(item.getName());
-			} else {
-				basketBean.setOrderNum(basketBean.getOrderNum());
 			}
 		}
 		basketBeanList.removeAll(toRemove);
 		session.setAttribute("basketBeans", basketBeanList);
-		model.addAttribute("stockShortages", stockShortages);
 		model.addAttribute("stockZeros", stockZeros);
+		model.addAttribute("stockShortages", stockShortages);
+		List<OrderItemBean> orderItemBeanList = new ArrayList<OrderItemBean>();
+		Integer total = 0;
 		for (BasketBean basketBean : basketBeanList) {
 			Item item = itemRepository.getById(basketBean.getId());
 			OrderItemBean orderItemBean = new OrderItemBean();
@@ -104,13 +115,14 @@ public class OrderRegistCustomerController {
 			orderItemBeanList.add(orderItemBean);
 			total += subtotal;
 		}
-		model.addAttribute("total", total);
 		model.addAttribute("orderItemBeans", orderItemBeanList);
+		model.addAttribute("total", total);
 		return "order/regist/order_check";
 	}
 	
 	@RequestMapping(path = "/order/complete", method = RequestMethod.POST)
 	public String completeOrder(OrderForm orderForm) {
+		
 		Order order = new Order();
 		BeanUtils.copyProperties(orderForm, order);
 		UserBean userBean = (UserBean)session.getAttribute("user");
@@ -119,10 +131,10 @@ public class OrderRegistCustomerController {
 		orderRepository.save(order);
 		List<BasketBean> basketBeanList = (ArrayList<BasketBean>)session.getAttribute("basketBeans");
 		for (BasketBean basketBean : basketBeanList) {
+			Item item = itemRepository.getById(basketBean.getId());
 			OrderItem orderItem = new OrderItem();
 			orderItem.setQuantity(basketBean.getOrderNum());
 			orderItem.setOrder(order);
-			Item item = itemRepository.getById(basketBean.getId());
 			orderItem.setItem(item);
 			orderItem.setPrice(item.getPrice());
 			orderItemRepository.save(orderItem);
