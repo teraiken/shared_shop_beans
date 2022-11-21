80e9c0373476df805aeff400ec03b28facd5a8d1
teraiken <terai.tis2022@gmail.com>
2022-11-21 12:51:43 +0900

    更新：OrderRegistCustomerController

--- src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
@@ -29,24 +29,52 @@ import jp.co.sss.shop.repository.OrderItemRepository;
 import jp.co.sss.shop.repository.OrderRepository;
 import jp.co.sss.shop.repository.UserRepository;
 
+/**
+ * 注文登録機能(一般会員用)のコントローラクラス
+ * 
+ * @author KenTerai
+ */
 @Controller
 public class OrderRegistCustomerController {
 	
+	/**
+	 * セッション
+	 */
 	@Autowired
 	HttpSession session;
 	
+	/**
+	 * 会員情報
+	 */
 	@Autowired
 	UserRepository userRepository;
 	
+	/**
+	 * 商品情報
+	 */
 	@Autowired
 	ItemRepository itemRepository;
 	
+	/**
+	 * 注文情報
+	 */
 	@Autowired
 	OrderRepository orderRepository;
 	
+	/**
+	 * 注文商品情報
+	 */
 	@Autowired
 	OrderItemRepository orderItemRepository;
 	
+	/**
+	 * 注文登録 届け先入力
+	 * 
+	 * @param orderForm 注文情報
+	 * @param backflg 　フラグ
+	 * @param model　　 Viewとの値受渡し
+	 * @return "order/regist/order_address_input" 注文登録 届け先入力画面へ
+	 */
 	@RequestMapping(path = "/address/input", method = RequestMethod.POST)
 	public String inputAddress(@ModelAttribute OrderForm orderForm, boolean backflg, Model model) {
 		
@@ -62,12 +90,26 @@ public class OrderRegistCustomerController {
 		return "order/regist/order_address_input";
 	}
 	
+	/**
+	 * 届け先情報入力エラー時
+	 * 
+	 * @return "order/regist/order_address_input" 注文登録 届け先入力画面へ
+	 */
 	@RequestMapping(path = "/address/input")
 	public String inputAddressRedirect() {
 		
 		return "order/regist/order_address_input";
 	}
 	
+	/**
+	 * 注文登録 支払方法選択
+	 * 
+	 * @param orderForm 注文情報
+	 * @param result　　入力チェック結果
+	 * @return
+	 *  入力値エラーあり："redirect:/address/input" 　　　　 注文登録 届け先入力画面へ
+	 *  入力値エラーなし："order/regist/order_payment_input" 注文登録 支払方法選択画面へ
+	 */
 	@RequestMapping(path = "/payment/input", method = RequestMethod.POST)
 	public String inputPayment(@Valid @ModelAttribute OrderForm orderForm, BindingResult result, RedirectAttributes redirectAttributes) {
 		
@@ -79,6 +121,13 @@ public class OrderRegistCustomerController {
 		return "order/regist/order_payment_input";
 	}
 	
+	/**
+	 * 注文登録 確認
+	 * 
+	 * @param orderForm 注文情報
+	 * @param model　　 Viewとの値受渡し
+	 * @return "order/regist/order_check" 注文登録 確認画面へ
+	 */
 	@RequestMapping(path = "/order/check", method = RequestMethod.POST)
 	public String checkOrder(@ModelAttribute OrderForm orderForm, Model model) {
 		
@@ -120,6 +169,12 @@ public class OrderRegistCustomerController {
 		return "order/regist/order_check";
 	}
 	
+	/**
+	 * 注文登録 完了
+	 * 
+	 * @param orderForm 注文情報
+	 * @return "order/regist/order_complete" 注文登録 完了画面へ
+	 */
 	@RequestMapping(path = "/order/complete", method = RequestMethod.POST)
 	public String completeOrder(OrderForm orderForm) {
 		
